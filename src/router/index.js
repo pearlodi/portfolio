148618c13262a1b36f6d7586366d9aea7a3030c0
pearlodi7@gmail.com <you@example.com>
2023-12-25 04:04:55 +0000

    add base url

--- src/router/index.js
+++ src/router/index.js
@@ -56,7 +56,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
