fbb5d0b45c9ec1eeaf9094ea074aa04889c91f09
odi pearl <89323758+pearlodi@users.noreply.github.com>
2024-02-04 01:29:20 +0100

    Update index.vue

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="project-background flex flex-col  md:h-full items-center p-2 md:p-8">
+    <div class="project-background flex flex-col  md:h-full items-center">
         <div class="flex flex-col  md:h-[100%] items-center w-[90%]">
             <div class="text-[40px] text-white w-[100%] max-w-full mx-auto mt-12 flex items-center pb-4">
                 <div class="line h-40 md:h-52 w-1">
