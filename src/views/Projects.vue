33ceb338fca6f6b0449e10044e4bf3b6803b8c79
odi pearl <89323758+pearlodi@users.noreply.github.com>
2024-04-22 22:19:23 +0100

    Update Projects.vue

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -61,7 +61,7 @@
                 <a :href="stack.link"
                   target="_blank">
                   <p class="text-[white] font-bold text-[12px] md:text-lg cursor-pointer">
-                    Visit {{ stack.company }} ➔ 
+                    Visit {{ stack.company }}
                   </p>
                 </a>
                 <p class="about-text mb-2 cursor-pointer">
