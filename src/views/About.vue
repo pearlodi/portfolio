91022d63b0c60a9c17457169a78ff7082e61c797
Odi Pearl <89323758+pearlodi@users.noreply.github.com>
2024-06-21 14:04:35 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -68,7 +68,7 @@
           </div>
         </div>
 
-        <div class="flex flex-wrap w-full max-w-full md:w-[900px]">
+        <div class="grid grid-cols-1 md:grid-cols-2 w-full max-w-full md:w-[900px]">
           <div
             v-for="category in categories"
             :key="category.title"
